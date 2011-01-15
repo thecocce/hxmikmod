8568e50b5639b51ab335b1df91f330cdf4734998
joukop <joukop@31651d87-ad8d-3f7b-c9a8-77638b1ffec8>
2011-01-15 10:58:31 +0000

    moved

---
+++ hxmikmod/FlashDriver.hx
@@ -0,0 +1,163 @@
+/**
+ *
+ * hxMikMod sound library
+ * Copyright (C) 2011 Jouko Pynnönen <jouko@iki.fi>
+ *             
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package hxmikmod;
+
+import flash.events.SampleDataEvent;
+import flash.media.SoundChannel;
+import flash.media.Sound;
+import hxmikmod.Types;
+import hxmikmod.SAMPLE;
+import hxmikmod.event.TrackerEvent;
+import hxmikmod.event.TrackerEventDispatcher;
+
+
+class FlashDriver extends MDriver {
+	var sound:Sound;
+	var channel:SoundChannel;
+
+	public function new(params:Hash<Dynamic>) {
+	   Name="FlashDriver for hxMikMod (c) jouko@iki.fi";
+	   Version=Name;
+	   HardVoiceLimit=1;
+	   SoftVoiceLimit=32;
+	   Virtch.buffer_size=4096;
+	   if (params!=null) try {
+	      if (params.exists("buffer_size")) Virtch.buffer_size=cast(params.get("buffer_size"));
+	   } catch(e:Dynamic) { trace(e); }
+	}
+
+
+
+	function onSampleData(event:SampleDataEvent):Void {
+           try {
+		//if (channel!=null)
+		//   TrackerEventDispatcher.setLatency(event.position/44.1-channel.position);
+                Virtch.WriteSamples(event.data);
+		TrackerEventDispatcher.dispatchEvent(new TrackerAudioBufferEvent(event.data));
+           } catch(e:Dynamic) { trace(e); }
+	}
+
+
+
+        override public function IsPresent():Bool {
+          return true;
+        }
+
+        override public function SampleLoad(s:SAMPLOAD,a:Int):SWORD {
+          return Virtch.VC_SampleLoad(s,a);
+        }
+
+        override public function FreeSampleSpace(a:Int):ULONG {
+           return Virtch.VC_SampleSpace(a);
+        }
+
+        override public function RealSampleLength(a:Int,s:SAMPLE):ULONG {
+           return Virtch.VC_SampleLength(a,s);
+        }
+
+        override public function Init():Bool {
+          return Virtch.VC_Init();
+        }
+
+        override public function Exit() {
+          trace("Exit");
+        }
+
+        override public function Reset():Bool {
+          trace("Reset");
+          return false;
+        }
+
+        override public function SetNumVoices():Bool {
+          return Virtch.VC_SetNumVoices();
+        }
+
+        override public function PlayStart():Bool {
+          if (sound==null) {
+		sound=new Sound();
+        	sound.addEventListener(SampleDataEvent.SAMPLE_DATA,onSampleData);
+        	channel=sound.play();
+	  }
+          return Virtch.VC_PlayStart();
+        }
+
+        override public function PlayStop():Void {
+        	if (sound!=null) {
+        	   sound.removeEventListener(SampleDataEvent.SAMPLE_DATA,onSampleData);
+        	   sound=null;
+		   channel=null;
+		}
+        }
+
+
+        override public function Pause():Void {
+          trace("Pause");
+        }
+
+        override public function VoiceSetVolume(a:UBYTE,b:UWORD):Void {
+           Virtch.VC_VoiceSetVolume(a,b);
+        }
+
+        override public function VoiceGetVolume(a:UBYTE):UWORD {
+           return Virtch.VC_VoiceGetVolume(a);
+        }
+
+        override public function VoiceSetFrequency(a:UBYTE,b:ULONG):Void {
+           Virtch.VC_VoiceSetFrequency(a,b);
+        }
+
+        override public function VoiceGetFrequency(a:UBYTE):ULONG {
+          trace("VoiceGetFrequency");
+          return 0;
+        }
+
+        override public function VoiceSetPanning(a:UBYTE,b:ULONG):Void {
+           Virtch.VC_VoiceSetPanning(a,b);
+        }
+
+        override public function VoiceGetPanning(a:UBYTE):ULONG {
+          trace("VoiceGetPanning");
+          return 0;
+        }
+
+        override public function VoicePlay(a:UBYTE,b:SWORD,c:ULONG,d:ULONG,e:ULONG,f:ULONG,g:UWORD):Void {
+           Virtch.VC_VoicePlay(a,b,c,d,e,f,g);
+        }
+
+        override public function VoiceStop(a:UBYTE):Void {
+           Virtch.VC_VoiceStop(a);
+	}
+
+        override public function VoiceStopped(a:UBYTE):Bool {
+            return Virtch.VC_VoiceStopped(a);
+        }
+
+        override public function VoiceGetPosition(a:UBYTE):SLONG {
+           return Virtch.VC_VoiceGetPosition(a);
+        }
+
+        override public function VoiceRealVolume(a:UBYTE):ULONG {
+          trace("VoiceRealVolume");
+          return 0;
+        }
+
+
+}
---
+++ hxmikmod/MODULE.hx
@@ -0,0 +1,101 @@
+/**
+ *
+ * hxMikMod sound library
+ * Copyright (C) 2011 Jouko Pynnönen <jouko@iki.fi>
+ *             
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package hxmikmod;
+
+import hxmikmod.Types;
+
+
+class MODULE {
+
+        /* general module information */
+	public var songname:String;
+	public var modtype:String;
+	public var comment:String;
+
+	public var flags:UWORD;
+	public var numchn:UBYTE;
+	public var numvoices:UBYTE;
+	public var numpos:UWORD;
+	public var numpat:UWORD;
+	public var numins:UWORD;
+	public var numsmp:UWORD;
+	public var instruments:Array<INSTRUMENT>;
+	public var samples:Array<SAMPLE>;
+	public var realchn:UBYTE;
+	public var totalchn:UBYTE;
+
+        /* playback settings */
+	public var reppos:UWORD;
+	public var initspeed:UBYTE;
+	public var inittempo:UWORD;
+	public var initvolume:UBYTE;
+	public var panning:Array<UWORD>;
+	public var chanvol:Array<UBYTE>;
+	public var bpm:UWORD;
+	public var sngspd:UWORD;
+	public var volume:SWORD;
+
+	public var extspd:Bool;
+	public var panflag:Bool;
+	public var wrap:Bool;
+	public var loop:Bool;
+	public var fadeout:Bool;
+
+	public var patpos:UWORD;
+	public var sngpos:SWORD;
+	public var sngtime:ULONG;
+	public var audiobufferstart:ULONG;
+
+	public var relspd:SWORD;
+
+
+        /* internal module representation */
+	public var numtrk:UWORD;
+	public var tracks:Array<Array<UBYTE>>;
+	public var patterns:Array<UWORD>;
+	public var pattrows:Array<UWORD>;
+	public var positions:Array<UWORD>;
+
+	public var forbid:Bool;
+	public var numrow:UWORD;
+	public var vbtick:UWORD;
+	public var sngremainder:UWORD;
+
+	public var control:Array<MP_CONTROL>;
+	public var voice:Array<MP_VOICE>;
+
+	public var globalslide:UBYTE;
+	public var pat_repcrazy:UBYTE;
+	public var patbrk:UWORD;
+	public var patdly:UBYTE;
+	public var patdly2:UBYTE;
+	public var posjmp:SWORD;
+	public var bpmlimit:UWORD;
+
+
+
+	public function new() {
+	   chanvol=new Array<UBYTE>();
+	   panning=new Array<UWORD>();
+	}
+
+}
+
